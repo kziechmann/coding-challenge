d304cc5e0f439fad2a067b7ed1b195400d8544f2
kziechmann <kziechmann@gmail.com>
2019-05-15 10:39:54 -0600

    basic component structure and state added for task groups and tasks.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
+import AllTasks from './Components/all_tasks.js'
+import TaskGroup from './Components/task_group'
+
 import './App.css'
 
 export default class App extends Component {
+  state = {
+    tasks: [],
+    current_group: 0
+  }
+
   render() {
-    return null
+    return this.state.current_group !== 0? <AllTasks></AllTasks> : <TaskGroup group_id={this.state.current_group}></TaskGroup>
   }
 }
---
+++ src/Components/task_group.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+
+export default class TaskGroup extends Component {
+  render() {
+    return <h1> TASK GROUP {this.props.group_id} </h1>
+  }
+}
